5c7b2970f9ea7d0605d314621e906e6fb3a01376
jacoban <jaco.banfi@gmail.com>
2017-08-22 16:31:43 +0200

    completed scripts for generating random instances on a given environment

--- connected_mrpp/scripts/build_batch_experiments.py
+++ connected_mrpp/scripts/build_batch_experiments.py
@@ -20,8 +20,6 @@ gflags.DEFINE_string('file_path', '../envs/offices.png', 'png file path')
 gflags.DEFINE_string('phys_discr_type', 'uniform_grid', 'environment discretization - physical')
 gflags.DEFINE_integer('cell_size', 11, 'pixels making 1 grid cell (only for uniform grid discretization)')
 
-gflags.DEFINE_string('comm_discr_type', 'range', 'environment discretization - comm')
-
 gflags.DEFINE_integer('min_range', 50, 'min communication range (in pixels)')
 gflags.DEFINE_integer('max_range', 201, 'max communication range (in pixels)')
 gflags.DEFINE_integer('step_range', 50, 'communication range step (in pixels)')
@@ -38,16 +36,22 @@ gflags.DEFINE_integer('min_dist', 200, 'minimum distance to travel between start
 
 gflags.DEFINE_bool('debug', True, 'debug mode active')
 
+comm_discr_types = ['los','range']
+
 def get_random_connected_config(G_C, size):
     connected = False
     config = random.sample(range(len(G_C.vs)), 1)
     while(len(config) < size):
-        candidates = G_C.neighbors(random.choice(config))
-        candidates_final = []
-        for cand in candidates:
-            if cand not in config:
-                candidates_final.append(cand)
-        config.append(random.choice(candidates_final))
+        neighbor_found = False
+        while(not(neighbor_found)):
+            candidates = G_C.neighbors(random.choice(config))
+            candidates_final = []
+            for cand in candidates:
+                if cand not in config:
+                    candidates_final.append(cand)
+            if (len(candidates_final) > 0):
+                neighbor_found = True
+                config.append(random.choice(candidates_final))
 
     return config
 
@@ -69,76 +73,78 @@ if __name__ == "__main__":
     argv = gflags.FLAGS(sys.argv)
     random.seed(1)
 
-    for max_dist in range(gflags.FLAGS.min_range, gflags.FLAGS.max_range, gflags.FLAGS.step_range):
-        print 'Range: ', max_dist
-        #create graphs file
-        os.system('python create_graph_from_png.py --file_path=' + gflags.FLAGS.file_path + \
-                  ' --phys_discr_type=' + gflags.FLAGS.phys_discr_type + \
-                  ' --cell_size=' + str(gflags.FLAGS.cell_size) + \
-                  ' --range=' + str(max_dist) + \
-                  ' --output_phys=' + gflags.FLAGS.output_phys + \
-                  ' --output_comm=' + gflags.FLAGS.output_comm + \
-                  ' --debug=False')
+    for comm_discr_type in comm_discr_types:
 
-        #read graphs files
-        phys_graph_file = gflags.FLAGS.output_phys.split('/')[2] + \
-                          '_' + gflags.FLAGS.phys_discr_type + \
-                          '_' + str(gflags.FLAGS.cell_size) + \
-                          '_' + gflags.FLAGS.comm_discr_type + \
-                          '_' + str(max_dist) + '.graphml'
-        comm_graph_file = gflags.FLAGS.output_comm.split('/')[2] + \
-                          '_' + gflags.FLAGS.phys_discr_type + \
-                          '_' + str(gflags.FLAGS.cell_size) + \
-                          '_' + gflags.FLAGS.comm_discr_type + \
-                          '_' + str(max_dist) + '.graphml'
+        for max_dist in range(gflags.FLAGS.min_range, gflags.FLAGS.max_range, gflags.FLAGS.step_range):
+            print 'Range: ', max_dist
+            #create graphs file
+            os.system('python create_graph_from_png.py --file_path=' + gflags.FLAGS.file_path + \
+                      ' --phys_discr_type=' + gflags.FLAGS.phys_discr_type + \
+                      ' --cell_size=' + str(gflags.FLAGS.cell_size) + \
+                      ' --range=' + str(max_dist) + \
+                      ' --output_phys=' + gflags.FLAGS.output_phys + \
+                      ' --output_comm=' + gflags.FLAGS.output_comm + \
+                      ' --debug=False')
 
-        G_E, G_C, im_array = get_graphs_and_image_from_files(phys_graph_file, comm_graph_file)
+            #read graphs files
+            phys_graph_file = gflags.FLAGS.output_phys.split('/')[2] + \
+                              '_' + gflags.FLAGS.phys_discr_type + \
+                              '_' + str(gflags.FLAGS.cell_size) + \
+                              '_' + comm_discr_type + \
+                              '_' + str(max_dist) + '.graphml'
+            comm_graph_file = gflags.FLAGS.output_comm.split('/')[2] + \
+                              '_' + gflags.FLAGS.phys_discr_type + \
+                              '_' + str(gflags.FLAGS.cell_size) + \
+                              '_' + comm_discr_type + \
+                              '_' + str(max_dist) + '.graphml'
 
-        shortest_paths = G_E.shortest_paths()
+            G_E, G_C, im_array = get_graphs_and_image_from_files(phys_graph_file, comm_graph_file)
 
-        if(gflags.FLAGS.debug):
-            fig = pl.figure()
-            ax = fig.add_subplot(111)
-            pl.ion()
-            pl.plot()
-            plot_plan(G_E, G_C, im_array, [], ax)
+            shortest_paths = G_E.shortest_paths()
 
-        for n_robots in range(2, gflags.FLAGS.max_robots + 1):
-            print 'N robots: ', n_robots
+            if(gflags.FLAGS.debug):
+                fig = pl.figure()
+                ax = fig.add_subplot(111)
+                pl.ion()
+                pl.plot()
+                plot_plan(G_E, G_C, im_array, [], ax)
 
-            for exp in range(gflags.FLAGS.n_exp):
-                print exp
-                #choose random start-goal for n_robots
-                ok = False
-                while(not(ok)):
-                    start_config = get_random_connected_config(G_C, n_robots)
-                    goal_config = get_random_connected_config(G_C, n_robots)
+            for n_robots in range(2, gflags.FLAGS.max_robots + 1):
+                print 'N robots: ', n_robots
 
-                    min_distance = min(map(lambda x: shortest_paths[start_config[x]][goal_config[x]], range(n_robots)))
+                for exp in range(gflags.FLAGS.n_exp):
+                    print exp
+                    #choose random start-goal for n_robots
+                    ok = False
+                    while(not(ok)):
+                        start_config = get_random_connected_config(G_C, n_robots)
+                        goal_config = get_random_connected_config(G_C, n_robots)
 
-                    if(min_distance > gflags.FLAGS.min_dist/gflags.FLAGS.cell_size): ok = True
+                        min_distance = min(map(lambda x: shortest_paths[start_config[x]][goal_config[x]], range(n_robots)))
 
-                #write new exp file
-                exp_name = gflags.FLAGS.file_path.split('/')[2].split('.')[0] + \
-                           '_' + gflags.FLAGS.phys_discr_type + \
-                           '_' + str(gflags.FLAGS.cell_size) + \
-                           '_' + gflags.FLAGS.comm_discr_type + \
-                           '_' + str(max_dist) + \
-                           '_' + str(n_robots) + \
-                           '_' + str(exp)
+                        if(min_distance > gflags.FLAGS.min_dist/gflags.FLAGS.cell_size): ok = True
 
-                write_exp_file(exp_name, phys_graph_file, comm_graph_file, start_config, goal_config)
-                print 'Experiment ', exp, ' created'
+                    #write new exp file
+                    exp_name = gflags.FLAGS.file_path.split('/')[2].split('.')[0] + \
+                               '_' + gflags.FLAGS.phys_discr_type + \
+                               '_' + str(gflags.FLAGS.cell_size) + \
+                               '_' + comm_discr_type + \
+                               '_' + str(max_dist) + \
+                               '_' + str(n_robots) + \
+                               '_' + str(exp)
 
-                if(gflags.FLAGS.debug):
-                    ax.cla()
-                    plot_plan(G_E, G_C, im_array, start_config, ax, color='bo')
-                    pl.draw()
-                    plt.pause(0.0001)
-                    time.sleep(1)
+                    write_exp_file(exp_name, phys_graph_file, comm_graph_file, start_config, goal_config)
+                    print 'Experiment ', exp, ' created'
 
-                    ax.cla()
-                    plot_plan(G_E, G_C, im_array, goal_config, ax, color='ro')
-                    pl.draw()
-                    plt.pause(0.0001)
-                    time.sleep(1)
+                    if(gflags.FLAGS.debug):
+                        ax.cla()
+                        plot_plan(G_E, G_C, im_array, start_config, ax, color='bo')
+                        pl.draw()
+                        plt.pause(0.0001)
+                        time.sleep(1)
+
+                        ax.cla()
+                        plot_plan(G_E, G_C, im_array, goal_config, ax, color='ro')
+                        pl.draw()
+                        plt.pause(0.0001)
+                        time.sleep(1)
--- connected_mrpp/scripts/create_graph_from_png.py
+++ connected_mrpp/scripts/create_graph_from_png.py
@@ -17,7 +17,7 @@ gflags.DEFINE_string('file_path', '../envs/offices.png', 'png file path')
 gflags.DEFINE_string('phys_discr_type', 'uniform_grid', 'environment discretization - physical')
 gflags.DEFINE_integer('cell_size', 11, 'pixels making 1 grid cell (only for uniform grid discretization)')
 
-gflags.DEFINE_string('comm_discr_type', 'range', 'environment discretization - comm')
+gflags.DEFINE_string('comm_discr_type', 'los', 'environment discretization - comm')
 gflags.DEFINE_integer('range', 150, 'communication range (in pixels)')
 
 gflags.DEFINE_string('output_phys', '../data/offices_phys', 'physical graph output')
@@ -36,6 +36,64 @@ def is_grid_cell(im_array, i, j, rows, cols):
 
     return True
 
+def directLinePossibleBresenham(start, end, im_array):
+    #print start
+    #print end
+    """Bresenham's Line Algorithm
+    Produces a list of tuples from start and end
+ 
+    >>> points1 = get_line((0, 0), (3, 4))
+    >>> points2 = get_line((3, 4), (0, 0))
+    >>> assert(set(points1) == set(points2))
+    >>> print points1
+    [(0, 0), (1, 1), (1, 2), (2, 3), (3, 4)]
+    >>> print points2
+    [(3, 4), (2, 3), (1, 2), (1, 1), (0, 0)]
+    """
+    # Setup initial conditions
+    x1, y1 = start
+    x2, y2 = end
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Determine how steep the line is
+    is_steep = abs(dy) > abs(dx)
+ 
+    # Rotate line
+    if is_steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+ 
+    # Swap start and end points if necessary and store swap state
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+ 
+    # Recalculate differentials
+    dx = x2 - x1
+    dy = y2 - y1
+ 
+    # Calculate error
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+ 
+    # Iterate over bounding box generating points between start and end
+    y = y1
+    
+    for x in range(x1, x2 + 1):
+        coord = (y, x) if is_steep else (x, y)
+        
+        if(im_array[coord] == 0): return False
+        
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+
+    return True
+
 def create_phys_graph_grid():
     print 'Creating grid physical graph...'
     im = cv2.imread(gflags.FLAGS.file_path)
@@ -46,12 +104,13 @@ def create_phys_graph_grid():
 
     G_E = Graph(directed=False)
     curr_id = 0
+    bu = gflags.FLAGS.cell_size/2
     for i in range(0, rows, gflags.FLAGS.cell_size):
         for j in range(0, cols, gflags.FLAGS.cell_size):
             if is_grid_cell(im_array, i,j, rows, cols):
                 G_E.add_vertex()
-                G_E.vs[curr_id]['y_coord'] = i
-                G_E.vs[curr_id]['x_coord'] = j
+                G_E.vs[curr_id]['y_coord'] = i + bu
+                G_E.vs[curr_id]['x_coord'] = j + bu
                 curr_id += 1
 
     neighbors = []
@@ -117,25 +176,70 @@ def create_comm_graph_range(G_E, im_array=None):
         plt.show()
     return G_C
 
+
+def create_comm_graph_los(G_E, im_array=None):
+    print 'Creating los communication graph...'
+    G_C = Graph(directed=False)
+    for vertex_id in range(len(G_E.vs)):
+        G_C.add_vertex()
+        G_C.vs[vertex_id]['x_coord'] = G_E.vs[vertex_id]['x_coord']
+        G_C.vs[vertex_id]['y_coord'] = G_E.vs[vertex_id]['y_coord']
+
+    neighbors = []
+    range_squared = gflags.FLAGS.range**2
+
+    for vertex_id_1 in range(len(G_C.vs)):
+        for vertex_id_2 in range(vertex_id_1 + 1, len(G_C.vs)):
+            if (G_C.vs[vertex_id_1]['x_coord']- G_C.vs[vertex_id_2]['x_coord'])**2 + \
+               (G_C.vs[vertex_id_1]['y_coord']- G_C.vs[vertex_id_2]['y_coord'])**2 <= range_squared:
+
+                if(directLinePossibleBresenham((G_C.vs[vertex_id_1]['y_coord'], G_C.vs[vertex_id_1]['x_coord']), 
+                                               (G_C.vs[vertex_id_2]['y_coord'], G_C.vs[vertex_id_2]['x_coord']), 
+                                               im_array)):
+                    neighbors.append((vertex_id_1, vertex_id_2))
+
+    G_C.add_edges(neighbors)
+
+    print 'Done.'
+
+    if gflags.FLAGS.debug:
+        plt.imshow(im_array)
+        for edge in G_C.es.select(_source=0):
+            v1 = G_C.vs[edge.source]
+            v2 = G_C.vs[edge.target]
+            plt.plot([v1['x_coord'], v2['x_coord']],[v1['y_coord'], v2['y_coord']],'b')
+        for edge in G_C.es.select(_target=0):
+            v1 = G_C.vs[edge.source]
+            v2 = G_C.vs[edge.target]
+            plt.plot([v1['x_coord'], v2['x_coord']],[v1['y_coord'], v2['y_coord']],'b')
+
+        plt.show()
+    return G_C
+
+
 if __name__ == "__main__":
     argv = gflags.FLAGS(sys.argv)
     if gflags.FLAGS.phys_discr_type == 'uniform_grid':
         G_E, im_array = create_phys_graph_grid()
-        G_E.write(gflags.FLAGS.output_phys + '_' + gflags.FLAGS.phys_discr_type + '_' + \
-                  str(gflags.FLAGS.cell_size) + '_' + gflags.FLAGS.comm_discr_type + '_' + \
-                  str(gflags.FLAGS.range) + '.graphml', format='graphml')
     else:
         print 'Error! Physical discretization not supported!'
         exit(1)
 
+    G_E.write(gflags.FLAGS.output_phys + '_' + gflags.FLAGS.phys_discr_type + '_' + \
+              str(gflags.FLAGS.cell_size) + '_' + gflags.FLAGS.comm_discr_type + '_' + \
+              str(gflags.FLAGS.range) + '.graphml', format='graphml')
+
     if gflags.FLAGS.comm_discr_type == 'range':
-        G_C = create_comm_graph_range(G_E, im_array)
-        G_C.write(gflags.FLAGS.output_comm + '_' + gflags.FLAGS.phys_discr_type + '_' + \
-                  str(gflags.FLAGS.cell_size) + '_' + gflags.FLAGS.comm_discr_type + '_' + \
-                  str(gflags.FLAGS.range) + '.graphml', format='graphml')
+        G_C = create_comm_graph_range(G_E, im_array)    
+    elif gflags.FLAGS.comm_discr_type == 'los':
+        G_C = create_comm_graph_los(G_E, im_array)
     else:
         print 'Error! Comm discretization not supported!'
         exit(1)
+
+    G_C.write(gflags.FLAGS.output_comm + '_' + gflags.FLAGS.phys_discr_type + '_' + \
+              str(gflags.FLAGS.cell_size) + '_' + gflags.FLAGS.comm_discr_type + '_' + \
+              str(gflags.FLAGS.range) + '.graphml', format='graphml')
 
 
 
